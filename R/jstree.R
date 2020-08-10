a8a085107eec52e0af069193640b1bf426afae35
stla <laurent_step@yahoo.fr>
2020-08-10 17:55:32 +0200

    jstree.R doc parameters

--- R/jstree.R
+++ R/jstree.R
@@ -16,16 +16,58 @@ NULL
 }
 
 
-#' <Add Title>
+#' HTML widget displaying an interactive tree
+#' @description Create a HTML widget displaying an interactive tree.
 #'
-#' <Add Description>
+#' @param nodes data, a list of nodes; each node is a list with a required
+#'   field \code{text}, a character string labeling the node, and optional
+#'   fields
+#'   \describe{
+#'     \item{\code{children}}{
+#'       a list of nodes
+#'     }
+#'     \item{\code{data}}{
+#'       a named list of data to attach to the node; see the
+#'       \link[jsTreeR:jstreeOutput]{Shiny examples}
+#'     }
+#'     \item{\code{icon}}{
+#'       space-separated HTML class names defining an icon, e.g.
+#'       \code{"glyphicon glyphicon-flash"}; in a Shiny app you can also use
+#'       a SuperTinyIcon, e.g. \code{"supertinyicon-julia"}; see the
+#'       \link[jsTreeR:jstreeOutput]{Shiny example} showing all available such
+#'       icons
+#'     }
+#'     \item{\code{type}}{
+#'       a character string for usage with the \code{types} option; see example
+#'     }
+#'   }
+#' @param elementId a HTML id for the widget (useless for common usage)
+#' @param checkboxes logical, whether to enable checkboxes next to each node;
+#'   this makes easier the selection of multiple nodes
+#' @param search logical, whether to enable the search functionality
+#' @param searchtime currently ignored
+#' @param dragAndDrop logical, whether to allow the rearrangement of the nodes
+#'   by dragging and dropping
+#' @param dnd a named list of JavaScript functions related to the drag-and-drop
+#'   functionality, e.g. the \code{is_draggable} function to define which nodes
+#'   are draggable; see the example and the
+#'   \href{https://www.jstree.com/api/}{jsTree API documentation} for the list
+#'   of possible functions
+#' @param types a named list of node properties; see example
+#' @param sort logical, whether to sort the nodes
+#' @param unique logical, whether to ensure that no node label is duplicated
+#' @param wholerow logical, whether to highlight whole selected rows
+#' @param contextMenu logical, whether to enable a context menu to create,
+#'   rename, delete, cut, copy and paste nodes
+#' @param checkCallback a JavaScript function; see the example where it is used
+#'   to define restrictions on the drag-and-drop behavior
 #'
 #' @import htmlwidgets
 #'
 #' @export
 jstree <- function(
-  data, width = NULL, height = NULL, elementId = NULL,
-  checkbox = FALSE,
+  nodes, elementId = NULL,
+  checkboxes = FALSE,
   search = FALSE, searchtime = 250,
   dragAndDrop = FALSE, dnd = NULL,
   types = NULL,
@@ -35,11 +77,10 @@ jstree <- function(
   contextMenu = FALSE,
   checkCallback = NULL
 ){
-
   # forward options using x
   x = list(
-    data = data,
-    checkbox = checkbox,
+    data = nodes,
+    checkbox = checkboxes,
     search = search,
     searchtime = searchtime,
     dragAndDrop = dragAndDrop,
@@ -56,8 +97,8 @@ jstree <- function(
   htmlwidgets::createWidget(
     name = 'jstree',
     x,
-    width = width,
-    height = height,
+    width = NULL,
+    height = NULL,
     package = 'jsTreeR',
     elementId = elementId,
     dependencies = htmltools::htmlDependency(
