14aa91a1f76fd81c289be20b6dec46ae3a6c226f
stla <laurent_step@yahoo.fr>
2020-09-23 23:34:01 +0200

    formatted trash.js

--- inst/htmlwidgets/gadget/trash.js
+++ inst/htmlwidgets/gadget/trash.js
@@ -1,32 +1,34 @@
 function getChildByText(parent, text) {
-  var texts = parent.children.map(function (child) {
+  var texts = parent.children.map(function(child) {
     return child.text;
   });
   var index = texts.indexOf(text);
-  if (index > -1) {
+  if(index > -1) {
     return parent.children[index];
   } else {
     return null;
   }
 }
+
 function restoreNode(tree, path, nodeAsJSON) {
   path = path.slice();
   path.shift();
   var parent = getNodesWithChildren(tree.get_json(), ["text", "id"])[0];
   var head = path.shift();
   var child = getChildByText(parent, head);
-  while (child !== null && path.length > 0) {
+  while(child !== null && path.length > 0) {
     parent = child;
     head = path.shift();
     child = getChildByText(parent, head);
   }
   path = [head].concat(path);
   var id = parent.id;
-  for (var i = 0; i < path.length - 1; i++) {
+  for(var i = 0; i < path.length - 1; i++) {
     id = tree.create_node(id, { text: path[i], type: "folder" });
   }
   tree.create_node(id, nodeAsJSON);
 }
+
 function restore(treeId, path, nodeAsJSON, id) {
   var tree = $("#" + treeId).jstree(true);
   restoreNode(tree, path, nodeAsJSON);
@@ -39,10 +41,11 @@ function restore(treeId, path, nodeAsJSON, id) {
     type: type
   });
 }
+
 function addTrashItem(treeId, path, nodeAsJSON) {
   var id = "node" + Math.random().toFixed(15).replace(".", "_");
   var onclick =
-    "var d = $(this).data(); " + "restore(d.instance, d.path, d.node, d.id)";
+    "var d = $(this).data(); restore(d.instance, d.path, d.node, d.id)";
   var attrs =
     "class='btn btn-success btn-sm btn-restore' " +
     `data-instance='${treeId}' ` +
