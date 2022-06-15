63bda33604fcf8008c16f7d5b9ac432aae81c3cf
stla <laurent_step@yahoo.fr>
2022-06-15 09:04:42 +0200

    doc fa icons

--- inst/essais/essai_curiousJorge.R
+++ inst/essais/essai_curiousJorge.R
@@ -4,42 +4,46 @@ nodes <- list(
   list(
     text = "Menu",
     state = list(opened = TRUE),
+    a_attr = list(style = "font-weight: bold;"),
     children = list(
       list(
-        text = "A",
+        text = "Dog",
         type = "moveable",
-        state = list(disabled = TRUE)
+        state = list(disabled = TRUE),
+        icon = "fas fa-dog"
       ),
       list(
-        text = "B",
+        text = "Cat",
         type = "moveable",
-        state = list(disabled = TRUE)
+        state = list(disabled = TRUE),
+        icon = "fas fa-cat"
       ),
       list(
-        text = "C",
+        text = "Fish",
         type = "moveable",
-        state = list(disabled = TRUE)
-      ),
-      list(
-        text = "D",
-        type = "moveable",
-        state = list(disabled = TRUE)
+        state = list(disabled = TRUE),
+        icon = "fas fa-fish"
       )
     )
   ),
   list(
-    text = "Drag here:",
+    text = ">>> Drag here <<<",
     type = "target",
-    state = list(opened = TRUE)
+    state = list(opened = TRUE),
+    a_attr = list(style = "font-weight: bold;")
   )
 )
 
 checkCallback <- JS(
-  "function(operation, node, parent, position, more) { console.log(node);",
+  "function(operation, node, parent, position, more) { ",
   "  if(operation === 'copy_node') {",
-  "    if(parent.id === '#' || node.parent !== 'j1_1' || parent.type !== 'target') {",
+  "    var n = parent.children.length;",
+  "    if(position !== n || parent.id === '#' || node.parent !== 'j1_1' || parent.type !== 'target') {",
   "      return false;", # prevent moving an item above or below the root
   "    }",               # and moving inside an item except a 'target' item
+  "  }",
+  "  if(operation === 'delete_node') {",
+  "    Shiny.setInputValue('deletion', position + 1);",
   "  }",
   "  return true;",      # allow everything else
   "}"
@@ -47,6 +51,7 @@ checkCallback <- JS(
 
 dnd <- list(
   always_copy = TRUE,
+  inside_pos = "last",
   is_draggable = JS(
     "function(node) {",
     "  return node[0].type === 'moveable';",
@@ -68,9 +73,88 @@ customMenu <- JS(
   "}")
 
 
-jstree(
+mytree <- jstree(
   nodes, dragAndDrop = TRUE, dnd = dnd, checkCallback = checkCallback,
   types = list(moveable = list(), target = list()),
   contextMenu = list(items = customMenu),
-  elementId = "mytree" # don't use elementId in Shiny! use the Shiny id
+  theme = "proton"
 )
+
+script <- '
+$(document).ready(function(){
+  var LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var Visited = {};
+  $("#mytree").on("copy_node.jstree", function(e, data){
+    var oldid = data.original.id;
+    var visited = Object.keys(Visited);
+    if(visited.indexOf(oldid) === -1){
+      Visited[oldid] = 0;
+    }else{
+      Visited[oldid]++;
+    }
+    var letter = LETTERS[Visited[oldid]];
+    var node = data.node;
+    var id = node.id;
+    var index = $("#"+id).index() + 1;
+    var text = index + ". " + node.text + " " + letter;
+    Shiny.setInputValue("choice", text);
+    var instance = data.new_instance;
+    instance.rename_node(node, text);
+  });
+});
+'
+
+library(shiny)
+ui <- fluidPage(
+  tags$head(tags$script(HTML(script))),
+  fluidRow(
+    column(
+      width = 4,
+      jstreeOutput("mytree")
+    ),
+    column(
+      width = 8,
+      verbatimTextOutput("choices")
+    )
+  )
+)
+
+server <- function(input, output, session){
+
+  output[["mytree"]] <- renderJstree(mytree)
+
+  Choices <- reactiveVal(data.frame(choice = character(0)))
+
+  observeEvent(input[["choice"]], {
+    Choices(
+      rbind(
+        Choices(),
+        data.frame(choice = input[["choice"]])
+      )
+    )
+  })
+
+  observeEvent(input[["deletion"]], {
+    Choices(
+      Choices()[-input[["deletion"]], , drop = FALSE]
+    )
+  })
+
+  output[["choices"]] <- renderPrint({
+    Choices()
+  })
+
+}
+
+shinyApp(ui, server)
+
+
+
+
+# browsable(
+#   tagList(
+#     jquerylib::jquery_core(),
+#     mytree,
+#     tags$script(HTML(script))
+#   )
+# )
