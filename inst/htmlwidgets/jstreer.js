9abfaae66f4020dab412626d9e16de1cc9cfda90
ismirsehregal <36849480+ismirsehregal@users.noreply.github.com>
2023-11-02 16:18:11 +0100

    Update jstreer.js
    Allow usage of relative css units for width

--- inst/htmlwidgets/jstreer.js
+++ inst/htmlwidgets/jstreer.js
@@ -146,8 +146,9 @@ function gridSearchBoxes(grid, id) {
     `<div id="${id}-searchFields" style="display: inline-block;">`;
   for(var i = 0; i < ncolumns; i++) {
     var column = columns[i];
-    var w = column.width - 2;
-    var style = `width: ${w}px; margin: 0 1px;`;
+	console.log(column.width);
+    var w = column.width;
+    var style = `width: calc(${w} - 2px); margin: 0 1px;`;
     var input =
       `<input type="text" name="${i}" value="" style="${style}">`;
     html += input;
