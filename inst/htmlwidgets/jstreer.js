d199614f5a202d519146d07a071c61931b7841a4
stla <laurent_step@yahoo.fr>
2024-02-14 13:16:40 +0100

    modif jstreegrid.min.js

--- inst/htmlwidgets/jstreer.js
+++ inst/htmlwidgets/jstreer.js
@@ -251,7 +251,6 @@ HTMLWidgets.widget({
         $el.jstree(options);
 
         $el.on("ready.jstree", function(e, data) {
-          $(".jstree-grid-wrapper").removeClass("shiny-bound-output");
           if(x.search) {
             if(x.grid) {
 
